1958b8c4df9eaf4b035890d04a0b111e13cd7764
SoftCancer <ygxzzu@163.com>
2020-03-14 19:06:16 +0800

    实现全局异常处理

---
+++ blog_common/src/main/java/com/dongl/exception/BaseExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.dongl.exception;
+
+import com.dongl.entity.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Description:  全局异常
+ * @author: YaoGuangXun
+ * @date: 2020/3/14 18:48
+ * @Version: 1.0
+ */
+@RestControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public Result exceptionHandler(Exception e){
+        e.printStackTrace();
+        return Result.exception(e.getMessage());
+    }
+}
