e7682be64745b3e766ec68d6c7503f352cc5a78b
SoftCancer <ygxzzu@163.com>
2020-03-19 16:09:28 +0800

    集成阿里云的短信发送服务，实现验证码发送。

---
+++ blog_user/src/main/java/com/dongl/user/sms/SmsListener.java
@@ -0,0 +1,50 @@
+package com.dongl.user.sms;
+
+import com.aliyuncs.exceptions.ClientException;
+import com.dongl.utils.SmsUtil;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @Description: 通过RabbitMQ 获取消息 实现短信发送
+ * 监听验证码队列
+ * @author: YaoGuangXun
+ * @date: 2020/3/19 15:20
+ * @Version: 1.0
+ */
+@Component
+@RabbitListener(queues = "checkcode.queue")
+public class SmsListener {
+
+    @Autowired
+    private SmsUtil smsUtil;
+
+    @Value("${aliyun.sms.template_code}")
+    private String template_code;
+
+
+    @Value("${aliyun.sms.sign_name}")
+    private String sign_name;
+
+
+    public void executeSms(Map<String ,String> map){
+        String mobile = map.get("mobile");
+        String checkCode = map.get("checkCode");
+
+        try {
+            /**
+             * code ：为阿里云 短信模板管理中的Code
+             * @Description: 短信发送功能
+             * @Author: YaoGuangXun
+             * @Date: 2020/3/19 15:29
+             **/
+            smsUtil.sendSms(mobile,template_code,sign_name,"{\"code\":\""+checkCode+"\"}");
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
