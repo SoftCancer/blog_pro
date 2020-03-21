a4d057a3c40aa40d1200da14d806df95d518628f
SoftCancer <ygxzzu@163.com>
2020-03-21 21:34:14 +0800

    整合 EurekaServer 服务端

---
+++ blog_eureka/src/main/java/com/dongl/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.dongl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @author: YaoGuangXun
+ * @date: 2020/3/21 18:59
+ * @Version: 1.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
